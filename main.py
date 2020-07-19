2baa4e8013178adea6a2bee8e42fc908bb99ef25
ravgeetdhillon <ravgeetdhillon@gmail.com>
2020-07-19 15:03:27 +0530

    keep everything in same file

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 import requests
 import os
 import json
-import utils
+
+
+def action_color(status):
+    """
+    Get a action color based on the workflow status.
+    """
+
+    if status == 'success':
+        return 'good'
+    elif status == 'failure':
+        return 'danger'
+
+    return 'warning'
+
+
+def action_status(status):
+    """
+    Get a transformed status based on the workflow status.
+    """
+
+    if status == 'success':
+        return 'passed'
+    elif status == 'failure':
+        return 'failed'
+
+    return 'passed with warnings'
+
+
+def action_emoji(status):
+    """
+    Get an emoji based on the workflow status.
+    """
+
+    if status == 'success':
+        return ':sunglasses:'
+    elif status == 'failure':
+        return ':worried:'
+
+    return ':zipper_mouth_face:'
 
 
 def notify_slack(job_status):
